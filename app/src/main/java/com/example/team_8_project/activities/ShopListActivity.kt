e57fe99c6383c43e427b344a0f37c9300fc941fd
MureaMarius <mureamarius18@yahoo.com>
2021-01-12 21:28:32 +0200

    Add Shopping list

--- app/src/main/java/com/example/team_8_project/activities/ShopListActivity.kt
+++ app/src/main/java/com/example/team_8_project/activities/ShopListActivity.kt
@@ -2,11 +2,33 @@ package com.example.team_8_project.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.team_8_project.R
 
-class ShopListActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class ShopListActivity : AppCompatActivity()
+{
+    private lateinit var txtViewList: TextView
+    private lateinit var editTextView: EditText
+    private lateinit var insertElement: Button
+
+    private lateinit var valueToPutInList: String
+    private lateinit var valueFromEditText: String
+    private var orderInList: Int = 1
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_list)
+
+        txtViewList = findViewById(R.id.txtViewList)
+        editTextView = findViewById(R.id.elementToAddInList)
+        insertElement = findViewById(R.id.btnInsertInList)
+
+        insertElement.setOnClickListener {
+            valueFromEditText = editTextView.text.toString()
+
+        }
     }
 }
