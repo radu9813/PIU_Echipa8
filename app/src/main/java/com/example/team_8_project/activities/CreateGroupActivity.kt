cf19acf579003c5840da0920a20b4987005d7f9e
Sechei Radu <radu9813@gmail.com>
2021-01-12 21:18:08 +0200

    WishList+GrupCreation

--- app/src/main/java/com/example/team_8_project/activities/CreateGroupActivity.kt
+++ app/src/main/java/com/example/team_8_project/activities/CreateGroupActivity.kt
@@ -2,6 +2,8 @@ package com.example.team_8_project.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.team_8_project.R
 
 class CreateGroupActivity : AppCompatActivity() {
@@ -9,4 +11,17 @@ class CreateGroupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_group)
     }
+
+    fun buttonPressedForFamily(view: View){
+        val toast = Toast.makeText(applicationContext, "Openning Phone Agenda to add family member to group", Toast.LENGTH_LONG)
+        toast.show()
+
+    }
+
+    fun buttonPressedForFriends(view: View){
+        val toast = Toast.makeText(applicationContext, "Openning Phone Agenda to add friend to group", Toast.LENGTH_LONG)
+        toast.show()
+
+    }
+
 }
